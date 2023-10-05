6f41affc68cb899db5131b715cbc290cc2e92a85
Sunny Kumar <113859373+sunnykumar26@users.noreply.github.com>
2023-10-06 03:37:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,9 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({
+    origin: ["http://localhost:5173", "https://inkspired.netlify.app"],
+    credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
